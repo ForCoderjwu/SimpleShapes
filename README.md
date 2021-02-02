91430b5e3a91b1e88c18c2c22e64edf99d95024a
ForCoderjwu <jwu85@dons.usfca.edu>
2021-02-03 07:05:16 +0800

    Upload the Main function

---
+++ README.md
@@ -0,0 +1,9 @@
+## Simple Shapes
+#### by Jiaquan Wu
+Feb 3th, 2021
+
+
+### Overview
+
+
+### Technical Details
---
+++ sketch.js
@@ -0,0 +1,10 @@
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0,255,127);
+}
